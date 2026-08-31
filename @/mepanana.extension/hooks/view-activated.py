db27f828a2421b0b3c24cd20a7909ec959a02a91
HHaii <125894628+nguyen-hoang-hai@users.noreply.github.com>
2026-08-31 16:58:40 +0700

    Delete @ directory

--- @/mepanana.extension/hooks/view-activated.py
+++
@@ -1,9 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-try:
-    from py.auth import update_ribbon_state, is_authenticated, get_current_user
-    if not is_authenticated():
-        update_ribbon_state(False)
-    else:
-        update_ribbon_state(True, get_current_user())
-except Exception:
-    pass
--- @/mepanana.extension/lib/py/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-mepanana lib/py — Shared Library
-"""
